b65cec3a4df75adc3c2abea6b85ad55c3ffc85ce
Edems <57250017+Edems-DEV@users.noreply.github.com>
2022-12-11 19:45:44 +0100

    🧹📄

--- MC_Clone/View/Components/FileEditor.cs
+++ MC_Clone/View/Components/FileEditor.cs
@@ -257,25 +257,11 @@ public class FileEditor : IComponent
         for (int i = 0; i < Rows.Count; i++)
         {
             int locMaxWidth = a;// - 1;
-            //if (Rows[i].Length > a)
-            //{
-                int lastLenght = Rows[i].Length - Cursor.X_offset;
-                if (lastLenght < locMaxWidth)
-                    locMaxWidth = lastLenght;
-                if(locMaxWidth < 0) { return; } //outside of offset
-                PrintRows[i] = Rows[i].Substring(Cursor.X_offset, locMaxWidth);
-            //}
-            //else
-            //{
-            //    if (Rows[i].Length <= Cursor.X_offset)
-            //    {
-            //        PrintRows[i] = " ";
-            //    }
-            //    else
-            //    {
-            //        PrintRows[i] = Rows[i];
-            //    }
-            //}
+            int lastLenght = Rows[i].Length - Cursor.X_offset;
+            if (lastLenght < locMaxWidth)
+                locMaxWidth = lastLenght;
+            if(locMaxWidth < 0) { return; } //outside of offset
+            PrintRows[i] = Rows[i].Substring(Cursor.X_offset, locMaxWidth);
         }
     }
     public void Enter()
